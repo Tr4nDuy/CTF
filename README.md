f3f076e246997d0f6610dac22d736945a0bd252e
Tr4nDuy <21522010@gm.uit.edu.vn>
2024-08-31 15:16:12 +0700

    add crypto CTF

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CTF
 
 ## picoCTF
+
 ![image](https://github.com/user-attachments/assets/dbe21317-d8e2-4ff0-83af-b2f16bc6e0d1)
---
+++ ThiTH MMH/bt7/sample_ecdsa.cpp
@@ -0,0 +1,238 @@
+//#include "stdafx.h"
+
+#include <assert.h>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "cryptopp/hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+
+#include <string>
+using std::string;
+
+#include "cryptopp/osrng.h"
+// using CryptoPP::AutoSeededX917RNG;
+using CryptoPP::byte;
+using CryptoPP::AutoSeededRandomPool;
+
+#include "cryptopp/aes.h"
+using CryptoPP::AES;
+
+#include "cryptopp/integer.h"
+using CryptoPP::Integer;
+
+#include "cryptopp/sha.h"
+using CryptoPP::SHA1;
+using CryptoPP::SHA256;
+
+#include "cryptopp/filters.h"
+using CryptoPP::StringSource;
+using CryptoPP::StringSink;
+using CryptoPP::ArraySink;
+using CryptoPP::SignerFilter;
+using CryptoPP::SignatureVerificationFilter;
+
+#include "cryptopp/files.h"
+using CryptoPP::FileSource;
+using CryptoPP::FileSink;
+
+#include "cryptopp/eccrypto.h"
+using CryptoPP::ECDSA;
+using CryptoPP::ECP;
+using CryptoPP::DL_GroupParameters_EC;
+
+//#if _MSC_VER <= 1200 // VS 6.0
+//using CryptoPP::ECDSA<ECP, SHA1>;
+//using CryptoPP::DL_GroupParameters_EC<ECP>;
+//#endif
+
+#include "cryptopp/oids.h"
+using CryptoPP::OID;
+#include<iostream>
+#include<fstream>
+#include "cryptopp/base64.h"
+using CryptoPP::Base64Decoder;
+using CryptoPP::Base64Encoder;
+//using namespace std;
+
+
+bool GeneratePrivateKey( const OID& oid, ECDSA<ECP, SHA1>::PrivateKey& key );
+bool GeneratePublicKey( const ECDSA<ECP, SHA1>::PrivateKey& privateKey, ECDSA<ECP, SHA1>::PublicKey& publicKey );
+
+void SavePrivateKey( const string& filename, const ECDSA<ECP, SHA1>::PrivateKey& key );
+void SavePublicKey( const string& filename, const ECDSA<ECP, SHA1>::PublicKey& key );
+void LoadPrivateKey( const string& filename, ECDSA<ECP, SHA1>::PrivateKey& key );
+void LoadPublicKey( const string& filename, ECDSA<ECP, SHA1>::PublicKey& key );
+
+void PrintDomainParameters( const ECDSA<ECP, SHA1>::PrivateKey& key );
+void PrintDomainParameters( const ECDSA<ECP, SHA1>::PublicKey& key );
+void PrintDomainParameters( const DL_GroupParameters_EC<ECP>& params );
+void PrintPrivateKey( const ECDSA<ECP, SHA1>::PrivateKey& key );
+void PrintPublicKey( const ECDSA<ECP, SHA1>::PublicKey& key );
+
+bool SignMessage( const ECDSA<ECP, SHA1>::PrivateKey& key, const string& message, string& signature );
+bool VerifyMessage( const ECDSA<ECP, SHA1>::PublicKey& key, const string& message, const string& signature );
+void loadFile2base64(const std::string& filename, string& mess) ;
+
+//////////////////////////////////////////
+// In 2010, use SHA-256 and P-256 curve
+//////////////////////////////////////////
+
+int main(int argc, char* argv[])
+{
+    // Scratch result
+    bool result = false;   
+    string signature;
+    
+    // Private and Public keys
+    ECDSA<ECP, SHA256>::PrivateKey privateKey;
+    ECDSA<ECP, SHA256>::PublicKey publicKey;
+    /////////////////////////////////////////////
+    // Load key in PKCS#9 and X.509 format     
+    LoadPrivateKey( "ec.private.key", privateKey );
+    LoadPublicKey( "ec.public.key", publicKey );
+    /////////////////////////////////////////////
+    // Sign and Verify a message   
+    string message ;
+    loadFile2base64("uit.png", message);
+    result = SignMessage( privateKey, message, signature );
+    assert( true == result );
+    string image;
+    StringSource (message,true,new Base64Decoder(new StringSink(image)));
+    // //cout << image << endl;
+
+    StringSource (image+signature,true , new FileSink( "md5.png", true /*binary*/ ));
+
+    return 0;
+}
+
+void loadFile2base64(const std::string& filename, string& mess) {
+    
+    FileSource file(filename.c_str(), true, new Base64Encoder(new StringSink(mess)));
+    file.PumpAll();
+    
+}
+
+bool GeneratePrivateKey( const OID& oid, ECDSA<ECP, SHA1>::PrivateKey& key )
+{
+    AutoSeededRandomPool prng;
+
+    key.Initialize( prng, oid );
+    assert( key.Validate( prng, 3 ) );
+     
+    return key.Validate( prng, 3 );
+}
+
+bool GeneratePublicKey( const ECDSA<ECP, SHA1>::PrivateKey& privateKey, ECDSA<ECP, SHA1>::PublicKey& publicKey )
+{
+    AutoSeededRandomPool prng;
+
+    // Sanity check
+    assert( privateKey.Validate( prng, 3 ) );
+
+    privateKey.MakePublicKey(publicKey);
+    assert( publicKey.Validate( prng, 3 ) );
+
+    return publicKey.Validate( prng, 3 );
+}
+
+void PrintDomainParameters( const ECDSA<ECP, SHA1>::PrivateKey& key )
+{
+    PrintDomainParameters( key.GetGroupParameters() );
+}
+
+void PrintDomainParameters( const ECDSA<ECP, SHA1>::PublicKey& key )
+{
+    PrintDomainParameters( key.GetGroupParameters() );
+}
+
+void PrintDomainParameters( const DL_GroupParameters_EC<ECP>& params )
+{
+    cout << endl;
+ 
+    cout << "Modulus:" << endl;
+    cout << " " << params.GetCurve().GetField().GetModulus() << endl;
+    
+    cout << "Coefficient A:" << endl;
+    cout << " " << params.GetCurve().GetA() << endl;
+    
+    cout << "Coefficient B:" << endl;
+    cout << " " << params.GetCurve().GetB() << endl;
+    
+    cout << "Base Point:" << endl;
+    cout << " X: " << params.GetSubgroupGenerator().x << endl; 
+    cout << " Y: " << params.GetSubgroupGenerator().y << endl;
+    
+    cout << "Subgroup Order:" << endl;
+    cout << " " << params.GetSubgroupOrder() << endl;
+    
+    cout << "Cofactor:" << endl;
+    cout << " " << params.GetCofactor() << endl;    
+}
+
+void PrintPrivateKey( const ECDSA<ECP, SHA1>::PrivateKey& key )
+{   
+    cout << endl;
+    cout << "Private Exponent:" << endl;
+    cout << " " << key.GetPrivateExponent() << endl; 
+}
+
+void PrintPublicKey( const ECDSA<ECP, SHA1>::PublicKey& key )
+{   
+    cout << endl;
+    cout << "Public Element:" << endl;
+    cout << " X: " << key.GetPublicElement().x << endl; 
+    cout << " Y: " << key.GetPublicElement().y << endl;
+}
+
+void SavePrivateKey( const string& filename, const ECDSA<ECP, SHA1>::PrivateKey& key )
+{
+    key.Save( FileSink( filename.c_str(), true /*binary*/ ).Ref() );
+}
+
+void SavePublicKey( const string& filename, const ECDSA<ECP, SHA1>::PublicKey& key )
+{   
+    key.Save( FileSink( filename.c_str(), true /*binary*/ ).Ref() );
+}
+
+void LoadPrivateKey( const string& filename, ECDSA<ECP, SHA1>::PrivateKey& key )
+{   
+    key.Load( FileSource( filename.c_str(), true /*pump all*/ ).Ref() );
+}
+
+void LoadPublicKey( const string& filename, ECDSA<ECP, SHA1>::PublicKey& key )
+{
+    key.Load( FileSource( filename.c_str(), true /*pump all*/ ).Ref() );
+}
+
+bool SignMessage( const ECDSA<ECP, SHA1>::PrivateKey& key, const string& message, string& signature )
+{
+    AutoSeededRandomPool prng;
+    
+    signature.erase();    
+
+    StringSource( message, true,
+        new SignerFilter( prng,
+            ECDSA<ECP,SHA1>::Signer(key),
+            new StringSink( signature )
+        ) // SignerFilter
+    ); // StringSource
+    
+    return !signature.empty();
+}
+
+bool VerifyMessage( const ECDSA<ECP, SHA1>::PublicKey& key, const string& message, const string& signature )
+{
+    bool result = false;
+
+    StringSource( signature+message, true,
+        new SignatureVerificationFilter(
+            ECDSA<ECP,SHA1>::Verifier(key),
+            new ArraySink( (byte*)&result, sizeof(result) )
+        ) // SignatureVerificationFilter
+    );
+
+    return result;
+}
